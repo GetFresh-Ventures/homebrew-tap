89d7bdebc8930eb57bba7db73ddc9037194eeaa4
iamdiraj <diraj@getfreshventures.com>
2026-06-18 19:45:41 -0700

    GIP-8841: Update formula to use standalone macOS binary

--- Formula/create-gxd.rb
+++ Formula/create-gxd.rb
@@ -1,16 +1,12 @@
 class CreateGxd < Formula
   desc "Growth by Design CEO AI Kit — Install an AI Chief of Staff in 3 minutes"
   homepage "https://www.getfreshventures.com/ceo-ai-kit"
-  url "https://github.com/GetFresh-Ventures/homebrew-tap/releases/download/v1.55.0/create-gxd-1.55.0.tgz"
+  url "https://github.com/GetFresh-Ventures/product-gxd-ceo-ai-kit/releases/download/v1.55.0/create-gxd-macos"
   version "1.55.0"
-  sha256 "e2d988ba146405faccea08b9b7561d67f6fafe0a9e79af01e5cfe51663bbe389"
-
-  depends_on "node@18"
+  sha256 "bc50e79605a83e25c8442b175512acf3398c3d8a545ef495dfae11d982fe29c1"
 
   def install
-    system "npm", "install", "--production"
-    libexec.install Dir["*"]
-    bin.install_symlink libexec/"index.js" => "create-gxd"
+    bin.install "create-gxd-macos" => "create-gxd"
   end
 
   test do
